1b95ff2422e3294641a8f28e4fc0e000c485a6b4
AlbertoVazquezDiaz <20223tn083@utez.edu.mx>
2025-05-26 17:44:49 -0600

    First commit

---
+++ src/main/java/org/example/cvavd9a/Cvavd9aApplication.java
@@ -0,0 +1,13 @@
+package org.example.cvavd9a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvavd9aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvavd9aApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cvavd9a/Cvavd9aApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cvavd9a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvavd9aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
